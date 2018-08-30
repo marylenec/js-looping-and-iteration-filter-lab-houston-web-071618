9146e0e5345c22a52f940cf1b49c529582a0b21b
Marylene Chan <marylenechan@Marylenes-MacBook-Pro-2.local>
2018-08-30 16:30:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, driver) {
+  driver = driver.toLowerCase()
+  // do end {}
+  const ans = drivers.filter(word => word.toLowerCase() == driver)
+  console.log(ans)
+  return ans
+}
+findMatching(drivers, "Sammy")
+
+// function findMatching2(drivers, driver) {
+//   const ans = drivers.filter(function (word) { return word == driver })
+//   console.log(ans)
+//   return ans
+// }
+// findMatching2(drivers, "Sammy")
+function fuzzyMatch(drivers) {
+  drivers = []
+  if drivers.startwith? {
+  console.log(drivers)
+  }
+}
+fuzzyMatch(drivers)
